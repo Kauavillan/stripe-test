e040a12315a097ae860c6ead864d943b6c4a3a99
Kaua Villa Nova <kaua.villa@prion.com.br>
2025-06-11 16:47:50 -0300

    Now using checkoutSession instead of payment intent to complete payments

--- components/CheckoutPage.tsx
+++ components/CheckoutPage.tsx
@@ -1,72 +1,67 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import {
+  CardElement,
+  CheckoutProvider,
+  ExpressCheckoutElement,
   PaymentElement,
+  useCheckout,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
-import React, { useEffect, useState } from "react";
 
-export default function CheckoutPage({ amount }: { amount: number }) {
-  const customerJWT =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYWRmY2E1OC01NTFiLTQ5NjAtYmY4Ny02Y2RjZWFiY2EyMzUiLCJpc3MiOiJodHRwOi8vbWFnaXBhc3MuY29tIiwic3ViIjoiMzM0YTM1MmItOTRhNi0xMWVmLWI4ZTgtMDI0MmFjMTIwMDAyIiwicm9sZSI6ImN1c3RvbWVyIiwiZGV2aWNlVHlwZSI6IndlYiIsImV4cCI6MTc0ODk2ODkxMiwiaWF0IjoxNzQ4OTY1MzEyLCJuYmYiOjE3NDg5NjUzMTJ9.kSAgznP_J6p0dOtQ-1S2Bpyb0v3cNInxwYjk1uyqhFE";
-  const stripe = useStripe();
-  const elements = useElements();
+export default function CheckoutPage() {
+  const checkout = useCheckout();
 
-  const [errorMessage, setErrorMessage] = useState<string>();
-  const [clientSecret, setClientSecret] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    fetch("http://localhost:5168/pass-purchases/intent-purchase", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${customerJWT}`,
-      },
-      body: JSON.stringify({
-        passId: "ca15e3e6-746a-11ef-8c2f-0242ac120002",
-        customerId: "1adc0af2-859f-11ef-8001-0242ac120002",
-      }),
-    })
-      .then((res) => res.text())
-      .then((data) => {
-        console.log(data);
-        setClientSecret(data);
-      });
-  }, [amount]);
+  const [coupon, setCoupon] = useState<string>(""); // novo estado para o cupom
+  checkout.total;
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    if (!stripe || !elements) {
-      return;
+  async function sendCoupon() {
+    const trimmedCoupon = coupon.trim();
+    if (trimmedCoupon) {
+      checkout.applyPromotionCode(trimmedCoupon);
     }
+  }
+  async function finishCheckout() {
     setLoading(true);
-    const { error: submitError } = await elements.submit();
-    if (submitError) {
-      setErrorMessage(submitError.message ?? "Unknown error");
-      setLoading(false);
-    } else {
-      // Confirm the payment with Stripe
-      const { error } = await stripe.confirmPayment({
-        elements,
-        clientSecret: clientSecret as string,
-        confirmParams: {
-          return_url: "http://localhost:3000/success",
-        },
-      });
-
-      if (error) {
-        setErrorMessage(error.message ?? "Unknown error");
-      }
-
+    try {
+      await checkout.confirm();
+    } catch (error) {
+      console.error("Erro no confirm do checkout:", error);
+    } finally {
       setLoading(false);
     }
   }
-
   return (
-    <form onSubmit={handleSubmit}>
-      {clientSecret && <PaymentElement />}
-      <button>Pay</button>
-    </form>
+    <div>
+      {/* Campo para permitir entrada do cupom */}
+      <input
+        type="text"
+        placeholder="Insira seu cupom"
+        value={coupon}
+        onChange={(e) => setCoupon(e.target.value)}
+      />
+      <button onClick={sendCoupon}>Enviar cupom</button>
+
+      <ExpressCheckoutElement onConfirm={() => console.log("Feito")} />
+      <PaymentElement />
+
+      <div>
+        <h1>Infos checkout</h1>
+
+        <div>
+          <span>Subtotal: {checkout.total.subtotal.amount}</span>
+        </div>
+        <div>
+          <span>Discount: {checkout.total.discount.amount}</span>
+        </div>
+        <div>
+          <span>Total: {checkout.total.total.amount}</span>
+        </div>
+        <button onClick={finishCheckout}>Finish</button>
+      </div>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,52 @@
 "use client";
 
-import { Elements } from "@stripe/react-stripe-js";
+import { CheckoutProvider, Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutPage from "../../components/CheckoutPage";
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
 }
-function convertToCents(amount: number) {
-  return Math.round(amount * 100);
-}
+
+const customerJWT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDk2NzI3MzYsImp0aSI6ImJlMzZiMWY2LTdmN2EtNDdiNy04ZjI3LTIzYWQyYzYwYzUxNiIsImlzcyI6Imh0dHBzOi8vYXBpLm1hZ2lwYXNzLmNvbSIsImF1ZCI6Imh0dHBzOi8vbWFnaXBhc3MuY29tIiwic3ViIjoiMWFkYzBhZjItODU5Zi0xMWVmLTgwMDEtMDI0MmFjMTIwMDAyIiwicm9sZSI6ImN1c3RvbWVyIiwiZGV2aWNlVHlwZSI6IndlYiIsImlhdCI6MTc0OTY2OTEzNiwibmJmIjoxNzQ5NjY5MTM2fQ.0-it2U_4x8qFXIuCork8IRDFEP1sK26ddQMmOhiOmg0";
+
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 export default function Home() {
+  async function fetchClientSecret() {
+    return fetch(
+      "http://localhost:5168/pass-purchases/create-payment-session",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${customerJWT}`,
+        },
+        body: JSON.stringify({
+          passId: "ca15e3e6-746a-11ef-8c2f-0242ac120002",
+          customerId: "1adc0af2-859f-11ef-8001-0242ac120002",
+          couponCode: "afteste10",
+        }),
+      }
+    )
+      .then((res) => res.text())
+      .then(async (data) => {
+        console.log("Client Secret:", data);
+        return data;
+      });
+  }
   const amount = 100;
   return (
     <div>
-      <Elements
+      <CheckoutProvider
         stripe={stripePromise}
         options={{
-          mode: "payment",
-          amount: convertToCents(amount),
-          currency: "usd",
+          fetchClientSecret: fetchClientSecret,
+          elementsOptions: { appearance: { theme: "flat" } },
         }}
       >
-        <CheckoutPage amount={amount} />
-      </Elements>
+        <CheckoutPage />
+      </CheckoutProvider>
     </div>
   );
 }
